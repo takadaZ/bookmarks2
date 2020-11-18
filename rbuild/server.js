c52f4ce28428be7ea15549eaf78e9a493e1b309a
Ubuntu <fumito80@gmail.com>
2020-11-18 17:47:38 +0000

    remote build server

--- rbuild/server.js
+++ rbuild/server.js
@@ -3,19 +3,28 @@
 const { src, dest } = require('gulp');
 const webpackStream = require('webpack-stream');
 const hashsum = require('gulp-hashsum');
+const archiver = require('archiver');
 const fs = require('fs');
 const http = require('http');
 const webpackConfig = require('../webpack.config.js');
 
-function cp() {
-  return src(['src/**/*.*', '!src/**/*.ts'])
-    .pipe(dest('dist'));
+const archive = archiver('zip', {
+  zlib: { level: 9 }, // Sets the compression level.
+});
+
+function pipeP(...fns) {
+  return (a) => fns.reduce((promise, f) => promise.then(f), Promise.resolve(a));
 }
 
-function webpack() {
-  return webpackStream(webpackConfig)
-    .pipe(dest('dist'));
-}
+// function cp() {
+//   return src(['src/**/*.*', '!src/**/*.ts'])
+//     .pipe(dest('dist'));
+// }
+
+// function webpack() {
+//   return webpackStream(webpackConfig)
+//     .pipe(dest('dist'));
+// }
 
 // function outputChecksum() {
 //   return src(['dist/**/*.*'])
@@ -39,13 +48,13 @@ function pipeToP(pipeF) {
 //   return fns.reduce((acc, f) => acc.then(f), f1());
 // }
 
-function parallel(...fns) {
-  return () => Promise.all(fns.map(pipeToP));
-}
+// function parallel(...fns) {
+//   return () => Promise.all(fns.map(pipeToP));
+// }
 
-const build = parallel(cp, webpack);
+// const build = parallel(cp, webpack);
 
-function rbuild(reject) {
+function webpack(reject) {
   return webpackStream(webpackConfig)
     .on('error', function err(error) {
       this.emit('end');
@@ -55,42 +64,59 @@ function rbuild(reject) {
     .pipe(dest('dist'))
     .pipe(src('dist/**/*.bundle.js'))
     .pipe(hashsum({
-      dest: './hashsum',
+      dest: './',
       filename: 'hashsum.json',
       // stream: true,
       json: true,
     }));
 }
 
-function respError(res, message) {
-  res.writeHead(500, { 'Content-Type': 'text/plane' });
-  res.end(message);
+async function build() {
+  const errBuild = await pipeToP(webpack).catch((reason) => reason);
+  if (errBuild) {
+    throw errBuild.message;
+  }
+  const [errHash, buff] = await new Promise((resolve) => {
+    fs.readFile('./hashsum.json', (err, buf) => resolve([err, buf]));
+  });
+  if (errHash) {
+    throw errHash.message;
+  }
+  return JSON.parse(buff);
+}
+
+function sorting(req) {
+  return async (remoteHash) => {
+    let body = '';
+    // eslint-disable-next-line no-return-assign
+    await pipeToP(() => req.on('data', (chunk) => body += chunk));
+    const localHash = JSON.parse(body);
+    const removes = Object.keys(localHash).filter((key) => !remoteHash[key]);
+    const updates = Object.entries(remoteHash)
+      .filter(([key, value]) => localHash[key] !== value)
+      .map(([key]) => key);
+    return [removes, updates];
+  };
 }
 
 function startServer() {
   return http.createServer(async (req, res) => {
     switch (req.url) {
       case '/build': {
-        const errBuild = await pipeToP(rbuild).catch((reason) => reason);
-        if (errBuild) {
-          return respError(res, errBuild.message);
-        }
-        const [errHash, buff] = await new Promise((resolve) => {
-          fs.readFile('./hashsum/hashsum.json', (err, buf) => resolve([err, buf]));
+        pipeP(
+          build,
+          sorting(req),
+          ([, updates]) => {
+            updates.forEach((filePath) => {
+              archive.file(filePath);
+            });
+            archive.pipe(res).on('end', () => res.end());
+            // res.writeHead(200, { 'Content-Type': 'application/json' });
+          },
+        )().catch((message) => {
+          res.writeHead(500, { 'Content-Type': 'text/plane' });
+          res.end(message);
         });
-        if (errHash) {
-          return respError(res, errHash.message);
-        }
-        const remoteHash = JSON.parse(buff);
-        let body = '';
-        // eslint-disable-next-line no-return-assign
-        await pipeToP(() => req.on('data', (chunk) => body += chunk));
-        const localHash = JSON.parse(body);
-        const removes = Object.keys(localHash).filter((key) => !remoteHash[key]);
-        const updates = Object.entries(remoteHash)
-          .filter(([key, value]) => localHash[key] !== value);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ updates, removes }));
         break;
       }
       default:
@@ -108,8 +134,8 @@ if (Number.isInteger(portNumber) && portNumber >= 80 && portNumber <= 65535) {
   server.listen(portNumber);
 }
 
-exports.cp = cp;
-exports.webpack = webpack;
-exports.startServer = startServer;
-exports.build = build;
+// exports.cp = cp;
+// exports.webpack = webpack;
+// exports.startServer = startServer;
+// exports.build = build;
 exports.pipeToP = pipeToP;
