e15d754bc561a28286be98edd0e488c1f523f102
unknown <fumito80@gmail.com>
2020-11-21 15:54:41 +0900

    remote build server

--- gulpfile.js
+++ gulpfile.js
@@ -25,3 +25,48 @@ const build = parallel(cp, webpack);
 exports.cp = cp;
 exports.webpack = webpack;
 exports.default = build;
+
+// test!
+
+const util = require('util');
+const filter = require('gulp-filter');
+const through2 = require('through2');
+const Vinyl = require('vinyl');
+
+async function getTextFromStream(strm) {
+  let text = '';
+  // eslint-disable-next-line no-return-assign
+  await pipeToP(() => strm.on('data', (chunk) => text += chunk));
+  return text;
+}
+
+function hook(vinyl, enc, done) {
+  console.log(vinyl);
+  this.push(vinyl);
+  done();
+}
+
+const file1 = new Vinyl({
+  cwd: '/',
+  base: '/src/',
+  path: '/src/file.js',
+  contents: Buffer.from('var x = 123'),
+});
+
+exports.test = async () => {
+  // console.log(file1);
+  const source = src(['src/**/*.*', '!src/**/*.ts', '!src/**/*.scss'])
+    .pipe(filter(['**/*.html']))
+    .pipe(through2.obj(function aa(vinyl, a2, done) {
+      this.push(vinyl);
+      done();
+    }, function es(cb) {
+      this.push(file1);
+      cb();
+    }))
+    .pipe(through2.obj(hook));
+    // .pipe(dest('./'));
+  // const text = await getTextFromStream(source);
+  // console.log(util.inspect(text, { depth: null }));
+  return source;
+};
